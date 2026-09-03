b557f2c7bb3da1640f34e87930ece48005f5d621
Shibfrite <kurekmaxmimilen@gmail.com>
2026-09-03 17:23:25 +0200

    tried to fix but mdb isnt working

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
 cat <<EOF >/tmp/init.sql
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
@@ -7,19 +10,12 @@ GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-exec mysqld --bind-address=0.0.0.0 --init-file=/tmp/init.sql
-
-until mysqladmin ping --silent; do
-  sleep 1
+service mariadb start
+while pgrep -x mysqld >/dev/null; do
+	sleep 1
 done
+mariadb < /tmp/init.sql
+service mariadb end
 
-echo "Ensuring database and user exist..."
-if ! mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`; \
-                       CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; \
-                       GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%'; \
-                       FLUSH PRIVILEGES;" 2>&1; then
-  echo "ERROR: Database creation failed" >&2
-  exit 1
-fi
-echo "Database setup completed."
-wait
+exec mysqld_safe
+#exec mysqld --bind-address=0.0.0.0 --init-file=/tmp/init.sql
