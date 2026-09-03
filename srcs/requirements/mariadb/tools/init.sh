dcca1602d88377085d42280944839a96c48420aa
Shibfrite <kurekmaximilien@gmail.com>
2026-09-03 15:51:28 +0200

    checks

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-exec mysqld_safe --bind-address=0.0.0.0 &
+cat <<EOF >/tmp/init.sql
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+exec mysqld --bind-address=0.0.0.0 --init-file=/tmp/init.sql
 
 until mysqladmin ping --silent; do
   sleep 1
